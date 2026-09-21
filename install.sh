eeb0654b760e39479cdcd76af9b561f368ca79ff
Majd Abu Diab <majdabudiab2013@gmail.com>
2026-09-21 12:21:00 +0300

    Avoid empty install environment paths

--- install.sh
+++ install.sh
@@ -31,8 +31,8 @@ cat > "$ENV_FILE" << 'EOF'
 export PATH="$HOME/.local/bin:$PATH"
 export XDG_DATA_DIRS="$HOME/.local/share:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
 export XDG_CONFIG_DIRS="$HOME/.local/etc/xdg:${XDG_CONFIG_DIRS:-/etc/xdg}"
-export LD_LIBRARY_PATH="$HOME/.local/lib:${LD_LIBRARY_PATH}"
-export QML2_IMPORT_PATH="$HOME/.local/lib/qml:${QML2_IMPORT_PATH}"
+export LD_LIBRARY_PATH="$HOME/.local/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export QML2_IMPORT_PATH="$HOME/.local/lib/qml${QML2_IMPORT_PATH:+:$QML2_IMPORT_PATH}"
 EOF
 
 # Make it executable
